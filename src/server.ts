dd935dc0b8eb823f56c08864e95256ead46d7029
Vijay Jangir <vijay_jangir4u@yahoo.com>
2024-08-28 04:25:50 +0530

    working vercel configuration

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import resourceRoutes from './routes/resourceRoutes';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Configure multer to store files in memory
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', resourceRoutes);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
